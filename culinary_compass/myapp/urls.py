7b3c6f1762c73ea13a6387f4c03cb129368b4ee9
110646 <yogibearysk1008@gmail.com>
2024-05-07 13:51:45 -0400

    made some chagnes

--- culinary_compass/myapp/urls.py
+++ culinary_compass/myapp/urls.py
@@ -1,3 +1,5 @@
+# Inside urls.py of your Django app
+
 from django.urls import path
 from . import views
 
@@ -5,8 +7,8 @@ app_name = 'myapp'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('results', views.results, name='results'),
-    path('details', views.details, name='details'),
+    path('results/', views.results, name='results'),  
+    path('details/', views.details, name='details'),
     path('about', views.about, name='about'),
     path('top', views.top, name='top'),
     path('tip', views.tip, name='tip')
--- culinary_compass/myapp/views.py
+++ culinary_compass/myapp/views.py
@@ -1,49 +1,41 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import render, reverse, redirect
+from django.shortcuts import render, redirect
 from django.conf import settings
+from urllib.parse import urlencode
+import requests
 
-
-from .mixins import (
-	FormErrors,
-	RedirectParams,
-	APIMixin
-)
-
-'''
-Basic view for selecting query
-'''
 def home(request):
+    if request.method == 'GET':
+        query = request.GET.get('query', '')
+        if query:
+            # If a query is provided, redirect to the results page
+            return redirect('myapp:results', query=query)
+        else:
+            # If no query is provided, render the home page template
+            return render(request, 'home.html')
 
-	if request.method == "POST":
-		query = request.POST.get("query", None)
-		if query:
-			return RedirectParams(url = 'myapp:results', params = {"query": query})
-
-	return render(request, 'home.html', {})
-
-
-
-'''
-Basic view for displaying results
-'''
 def results(request):
-
-	query = request.GET.get("query", None)
-
-	if query:
-		results = APIMixin( query=query).get_data()
-		if results:
-			context = {
-				"results": results,
-				"query": query,
-			}
-			return render(request, 'results.html', context)
-	
-	return redirect(reverse('myapp:home'))
+    query = request.GET.get('query', '')
+    url = f"https://api.spoonacular.com/recipes/complexSearch?query={query}&apiKey={settings.API_KEY}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        results = data.get('results', [])
+        return render(request, 'results.html', {'results': results, 'query': query})
+    else:
+        return render(request, 'results.html', {'results': [], 'query': query})
 
 def details(request):
-    return render(request, 'details.html', {})
+    recipe_id = request.GET.get('recipe_id', '')
+    url = f'https://api.spoonacular.com/recipes/{recipe_id}/information'
+    params = {
+        'apiKey': settings.API_KEY,
+    }
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        recipe_data = response.json()
+        return render(request, 'details.html', {'recipe': recipe_data})
+    else:
+        return render(request, 'details.html', {'recipe': None})
 
 def about(request):
     return render(request, 'about.html', {})
