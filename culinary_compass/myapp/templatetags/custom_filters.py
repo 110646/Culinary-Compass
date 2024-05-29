147b4faca191032cc95f325ac1f045ad433691e2
110646 <yogibearysk1008@gmail.com>
2024-05-29 14:20:50 -0400

    made some changes to the details page

--- culinary_compass/myapp/templatetags/custom_filters.py
+++ culinary_compass/myapp/templatetags/custom_filters.py
@@ -8,3 +8,19 @@ def half(value):
         return int(value) // 2
     except (ValueError, TypeError):
         return value
+
+@register.filter
+def add_newlines(value):
+    if not isinstance(value, str):
+        return value
+    # Split the text at each period followed by a space, then join with <br>
+    return value.replace('.', '.<br>')
+
+@register.filter
+def is_essential_nutrient(nutrient_name):
+    essential_nutrients = [
+        "Calories", "Fat", "Protein", "Sodium", "Sugar", "Vitamin A", "Vitamin C",
+        "Calcium", "Iron", "Vitamin D", "Vitamin E", "Vitamin K", "Vitamin B6",
+        "Vitamin B12", "Magnesium", "Zinc"
+    ]
+    return nutrient_name in essential_nutrients
